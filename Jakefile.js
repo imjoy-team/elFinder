208bd0c7ff6dbb903ee013f2f95d757ce41741f7
Wei Ouyang <oeway007@gmail.com>
2020-04-19 16:28:23 +0200

    Add package.json to installation

--- Jakefile.js
+++ Jakefile.js
@@ -374,6 +374,7 @@ task('misc', function(){
 		.concat(files['misc'])
 		.concat(path.join(src, 'files', '.gitignore'))
 		.concat(path.join(src, 'files', '.trash', '.gitignore'))
+		.concat(path.join(src, 'package.json'))
 		.concat(path.join(src, '__init__.py'));
 	for (i in cf)
 	{
