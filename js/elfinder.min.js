7d766e7714b0a1073503b16ab017016d4184fa34
oeway <oeway@users.noreply.github.com>
2021-03-01 00:44:14 +0000

    Deploy to GitHub Pages 59c3fbf5fb405ac3d68986e3a52df81bade74bd1

--- js/elfinder.min.js
+++ js/elfinder.min.js
@@ -1,6 +1,6 @@
 /*!
  * elFinder - file manager for web
- * Version 2.1.56 (2021-02-28)
+ * Version 2.1.56 (2021-03-01)
  * http://elfinder.org
  * 
  * Copyright 2009-2021, Studio 42
---
+++ js/lib/service-worker.js
@@ -0,0 +1,62 @@
+/* eslint no-unused-vars: 0 */
+/* global importScripts, ServiceWorkerWare */
+importScripts('./js/lib/ServiceWorkerWare.js');
+
+let getVersionPort=null;
+const requestPool = {};
+const routeRegistry = {};
+const worker = new ServiceWorkerWare();
+self.addEventListener("message", event => {
+  if (event.data && event.data.type === 'INIT_PORT') {
+    getVersionPort = event.ports[0];
+    const clientId = event.data.clientId;
+    routeRegistry[clientId] = {}
+   
+  }
+  else if(event.data && event.data.type === 'REGISTER'){
+    const routes = event.data.routes || []
+    const clientId = event.data.clientId;
+    for(let route of routes){
+      worker[route.type]('/'+clientId+route.path, async function(req) {
+        try{
+          const response = await makeRequest(clientId, {route, parameters: req.parameters, body: req.body})
+          if(response.error){
+            return new Response(`${response.error}`, {status: response.status || 500, statusText: response.statusText || 'ERROR'})
+          }
+          return new Response(response.body, {status: response.status || 200, statusText: response.statusText || 'OK', headers: response.headers || {}});
+        }
+        catch(e){
+          console.error(e)
+        }
+      });
+    }
+  }
+  else if (event.data && event.data.type === 'RESPONSE') {
+    // getVersionPort.postMessage({ payload: ++count });
+    if(event.data.requestId && requestPool[event.data.requestId]){
+      const promise = requestPool[event.data.requestId]
+      delete requestPool[event.data.requestId]
+      promise.resolve(event.data.response)
+    }
+  }
+})
+
+function makeRequest(clientId, request){
+  return new Promise((resolve, reject)=>{
+    if(!getVersionPort) throw new Error("API communication is not initialized.")
+    const requestId = `${Date.now()}`;
+    requestPool[requestId] = {resolve, reject}
+    //TODO: implement timeout for requests
+    getVersionPort.postMessage({ type: 'REQUEST', requestId: requestId, request, clientId });
+  })
+}
+
+worker.get('/status', async function(req, res) {
+  return new Response("Ready", {status: 200, statusText: 'OK'});
+});
+
+
+// Start the service worker.
+worker.init();
+
+console.log("In-browser proxy server is running ")
