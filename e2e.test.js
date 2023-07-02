4b5d8cc9d75500389fcb38cd317770b2537fe941
oeway <oeway007@gmail.com>
2023-07-02 13:49:04 +0200

    Fix playwright install

--- e2e.test.js
+++ e2e.test.js
@@ -2,5 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('can interact with elFinder in imjoy', async ({ page }) => {
   await page.goto('http://localhost:4000/test.html'); // replace with your test page URL
-  await page.waitForSelector('#nav-v2_Lw');
+  await page.waitForSelector('#report-1');
+  await page.waitForSelector('#report-2');
+  await page.waitForSelector('#report-3');
 });
